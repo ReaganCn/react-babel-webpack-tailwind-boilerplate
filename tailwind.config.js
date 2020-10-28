92a2f9f4c33602bcc5826e5fe37437ae905b38a7
ReaganCn <rgncharana2@gmail.com>
2020-10-28 11:27:30 +0300

    First commit for this boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    future: {},
+    purge: [],
+    theme: {
+      extend: {
+        animation: {
+          "spin-slow": "spin 4s linear infinite",
+        },
+        colors: {
+          "react-blue": "#00d8ff",
+        },
+      },
+      fontFamily: {
+        "space-mono": ["Space Mono"],
+      },
+    },
+    variants: {},
+    plugins: [],
+  };
